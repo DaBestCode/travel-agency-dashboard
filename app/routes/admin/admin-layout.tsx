6fbb9ad90bc808563d5610eacb1bfc01d7506875
dabestcode <pjanga@asu.edu>
2026-01-17 22:58:59 -0700

    layout setup

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet } from "react-router";
+import pkg from "@syncfusion/ej2-react-navigations";
+const { SidebarComponent } = pkg;
+import { NavItems, MobileSidebar } from "../../../components";
+
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      <MobileSidebar className="sm:block lg:hidden" />
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  );
+};
+
+export default AdminLayout;
---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileSidebar = ({ className }: { className: string }) => {
+  return <div className={className}>MobileSidebar</div>;
+};
+
+export default MobileSidebar;
